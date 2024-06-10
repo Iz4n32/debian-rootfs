a49cb2fbd43fb7018421b694ae06176ed1938063
Izan <izan@example.com>
2024-06-10 10:43:17 +0200

    Separar set_chroot y unset_chroot en funciones

--- 1-create-rootfs.sh
+++ 1-create-rootfs.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+
+set_chroot () {
+
+	# Copy qemu binary to rootfs
+	cp $qemu_path $build_dir/$rootfs_dir_utc$qemu_path
+
+	# Mount /dev in rootfs
+	mount --bind /dev $build_dir/$rootfs_dir_utc/dev
+
+	# Complete the configure of dash
+	chroot $build_dir/$rootfs_dir_utc /var/lib/dpkg/info/dash.preinst install
+
+	# Configure debian packages
+	chroot $build_dir/$rootfs_dir_utc dpkg --configure -a
+}
+
+unset_chroot () {
+
+	# Kill processes running in rootfs
+	fuser -sk $build_dir/$rootfs_dir_utc
+
+	# Remove qemu binary from rootfs
+	rm $build_dir/$rootfs_dir_utc$qemu_path 2>/dev/null
+
+	# Umount /dev in rootfs
+	umount $build_dir/$rootfs_dir_utc/dev
+}
+
+
 # Check architecture and set variables
 if [[ ! $check_and_set ]]; then
     . 0-check-and-set.sh $1 $2
@@ -41,17 +70,7 @@ else
     export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
     export LC_ALL=C LANGUAGE=C LANG=C
 
-    # Copy qemu binary to rootfs
-    cp $qemu_path $build_dir/$rootfs_dir_utc$qemu_path
-
-    # Mount /dev in rootfs
-    mount --bind /dev $build_dir/$rootfs_dir_utc/dev
-
-    # Complete the configure of dash
-    chroot $build_dir/$rootfs_dir_utc /var/lib/dpkg/info/dash.preinst install
-
-    # Configure debian packages
-    chroot $build_dir/$rootfs_dir_utc dpkg --configure -a
+	set_chroot
 fi
 
 # Empty root password
@@ -60,14 +79,8 @@ chroot $build_dir/$rootfs_dir_utc passwd -d root
 # Get packages installed
 chroot $build_dir/$rootfs_dir_utc dpkg -l | awk '{if (NR>3) {print $2" "$3}}' > $build_dir/$rootfs_dir_utc\-packages
 
-# Kill processes running in rootfs
-fuser -sk $build_dir/$rootfs_dir_utc
-
-# Remove qemu binary from rootfs
-rm $build_dir/$rootfs_dir_utc$qemu_path 2>/dev/null
-
-# Umount /dev in rootfs
-umount $build_dir/$rootfs_dir_utc/dev
+unset_chroot
 
 # Latest rootfs is the current one
 ln -sfn $rootfs_dir_utc $build_dir/$rootfs_dir
+
