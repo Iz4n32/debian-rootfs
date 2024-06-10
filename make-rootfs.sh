f9e152f50a3096a22fd3fdceeb984a1641cfeac0
Izan <izan@example.com>
2024-06-10 14:28:49 +0200

    echo time elapsed when finished

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+START_MINUTE=$(date "+%H*60+%M" | bc )
+
+
 . 0-check-and-set.sh $1 $2
 . 1-create-rootfs.sh
 . 2-configure-rootfs.sh
 . 3-archive-rootfs.sh
+
+
+echo "Time elapsed: $(($(date "+%H*60+%M" | bc )-$START_MINUTE)) minutes"
+
