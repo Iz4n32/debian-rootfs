832a4bee83194306979b9941d20f560624f0ccd1
iz4n32 <izan32@gmail.com>
2026-04-26 08:18:38 +0200

    Subido installer_multistrap_U26.sh para instalar el multistrap 2.2.11

---
+++ multistrap_2.2.10-11_patch/installer_multistrap_U26.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install libconfig-auto-perl libparse-debian-packages-perl || exit 1
+
+wget https://archive.ubuntu.com/ubuntu/pool/universe/m/multistrap/multistrap_2.2.11_all.deb || exit 1
+
+sudo dpkg -i multistrap_2.2.11_all.deb || exit 1
+
+exit 0
+
